3048faa8690ed7113e58fe60d5a89295a9cfd9ad
akbarul <akbarul@qiscus.co>
2017-10-06 03:23:26 +0700

    step 1-create an unit test

---
+++ TheRegistrationPageTests/TheRegistrationPageTests.swift
@@ -0,0 +1,46 @@
+//
+//  TheRegistrationPageTests.swift
+//  TheRegistrationPageTests
+//
+//  Created by qiscus on 10/6/17.
+//  Copyright © 2017 technomotion. All rights reserved.
+//
+
+import XCTest
+@testable import TheRegistrationPage
+
+class TheRegistrationPageTests: XCTestCase {
+    
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testRegisterWithAllFilled(){
+        XCTFail()
+    }
+    
+    func testRegisterWithEmptyEmail(){
+        XCTFail()
+    }
+    
+    func testRegistrationWithEmptyPassword(){
+        XCTFail()
+    }
+    
+    func testRegistrationWithEmptyAge(){
+        XCTFail()
+    }
+    
+    func testRegistrationWithEmptyPhoneNumber(){
+        XCTFail()
+    }
+    
+    
+}
