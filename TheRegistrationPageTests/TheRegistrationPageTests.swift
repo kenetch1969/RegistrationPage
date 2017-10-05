48294ff81801c9975c16c6bf83c987da820b69e0
akbarul <akbarul@qiscus.co>
2017-10-06 03:24:45 +0700

    step2-create the outcome initiation of registrationPresenter

--- TheRegistrationPageTests/TheRegistrationPageTests.swift
+++ TheRegistrationPageTests/TheRegistrationPageTests.swift
@@ -11,9 +11,11 @@ import XCTest
 
 class TheRegistrationPageTests: XCTestCase {
     
+    var registrationPresenter: RegistrationPresenter?
     
     override func setUp() {
         super.setUp()
+        self.registrationPresenter = RegistrationPresenter(delegate: self)
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
